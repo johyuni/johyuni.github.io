f191eb36712c3b2b942343879280e6411c013ffe
johyuni <2252hyun@gmail.com>
2024-12-13 22:38:25 +0900

    font setting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // style
-import "./asset/styles/style.scss";
+import "./assets/styles/style.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>준비중입니다. </p>
+        <p>안녕하세용?</p>
+        <span>신애정잡놈</span>
       </header>
     </div>
   );
