e64b32abdeecb006e65f7939b28bac6f6eed9229
johyuni <2252hyun@gmail.com>
2025-01-07 20:09:43 +0900

    페이지 스타일 파일 생성  / 메인비주얼 텍스트 구성

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,32 +2,37 @@
 // todo :: nav를 ui>li>a 구조  아님 nav>a 구조?
 // todo :: 메뉴를 클릭할때 해당섹션으로 부드럽게 포커스되어야함
 // todo :: 이 컴포넌트를 app.js에 연결해서 메인페이지에 보이도록 하기
+// todo :: .header__nav 필요할지?
 
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
-      <Link to="/" aria-label="상위로 이동">
+      <a href="/" role="button" aria-label="상위로 이동">
         <h1 className="header__logo">
           <span className="blind">Portfolio</span>
         </h1>
-      </Link>
+      </a>
 
       <nav className="header__nav">
-        <Link to="/" className="header__link" aria-label="프로필">
+        <a href="/" role="button" className="header__link" aria-label="프로필">
           Profile
-        </Link>
-        <Link to="/" className="header__link" aria-label="프로젝트">
+        </a>
+        <a
+          href="/"
+          role="button"
+          className="header__link"
+          aria-label="프로젝트"
+        >
           Project
-        </Link>
-        <Link to="/" className="header__link" aria-label="노트">
+        </a>
+        <a href="/" role="button" className="header__link" aria-label="노트">
           Notes
-        </Link>
-        <Link to="/" className="header__link" aria-label="연결">
+        </a>
+        <a href="/" role="button" className="header__link" aria-label="연결">
           Connect
-        </Link>
+        </a>
       </nav>
     </header>
   );
--- src/components/pages/Visual.js
+++ src/components/pages/Visual.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Visual() {
-  return <section className="main__visual"></section>;
+  return (
+    <section className="main__visual">
+      <h2 className="text-box">
+        <p className="title">Portfolio</p>
+        <span className="dsc">조현희의 포트폴리오입니다.</span>
+      </h2>
+    </section>
+  );
 }
 
 export default Visual;
