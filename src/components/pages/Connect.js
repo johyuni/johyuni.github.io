8e2bf9cb4463ef1ec997e3821a556e56047f9968
johyuni <2252hyun@gmail.com>
2025-02-03 17:14:57 +0900

    Profile 영역 마크업

--- src/components/pages/Connect.js
+++ src/components/pages/Connect.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Connect() {
-  return <section className="main__connect"></section>;
+  return (
+    <section className="main__connect">
+      {/* <div>
+        <span>jo hyunhee</span>
+        <span>&#43;82&#41; 10&#45;4068&#45;2252</span>
+        <span>2252hyun&#64;gmail.com</span>
+        <span>&#64;iiuoahy</span>
+      </div> */}
+    </section>
+  );
 }
 
 export default Connect;
--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,7 +1,101 @@
 import React from "react";
 
 function Profile() {
-  return <section className="main__profile"></section>;
+  return (
+    <section className="main__profile">
+      <div className="inner">
+        {/* left */}
+        <div className="">
+          <strong>안녕하세요 퍼블리셔 조현희입니다</strong>
+          <div>
+            <strong>jo hyunhee</strong>
+            <p>
+              간단한 소개글 영역입니다 간단한 소개글 영역입니다 간단한 소개글
+              영역입니다 간단한 소개글 영역입니다
+            </p>
+          </div>
+        </div>
+
+        {/* right */}
+        <div className="">
+          <div>
+            <strong>Information</strong>
+            <span>1995.02.06</span>
+            <span>&#43;82&#41; 10&#45;4068&#45;2252</span>
+            <span>2252hyun&#64;gmail.com</span>
+          </div>
+
+          <div>
+            <strong>Education</strong>
+            <dl>
+              <dt>2022</dt>
+              <dd>
+                반응형 웹디자인 &#40;UIUX&#41; &#38; 웹퍼블리셔 디지털 실무
+                양성과정 &#40;B&#41; 수료
+              </dd>
+              <dt>2015</dt>
+              <dd>한국 폴리텍Ⅱ대학 인천캠퍼스 졸업</dd>
+              <dt>2013</dt>
+              <dd>경인여자고등학교 졸업</dd>
+            </dl>
+          </div>
+
+          <div>
+            <strong>Experience</strong>
+            <dl>
+              <dt>2023. 11 &#126; 2024. 09</dt>
+              <dd>주식회사 버치홀딩스 퍼블리셔</dd>
+              <dt>2022. 05 &#126; 2023. 05</dt>
+              <dd>주식회사 인스플래닛 퍼블리셔</dd>
+              <dt>2021. 02 &#126; 2021. 08</dt>
+              <dd>제주항공 B2C 홈페이지 리뉴얼 프리랜서 디자이너</dd>
+              <dt>2018. 04 &#126; 2020. 09</dt>
+              <dd>주식회사 위아트 디자이너</dd>
+            </dl>
+          </div>
+
+          <div>
+            <strong>Interest in</strong>
+            <span>퍼블리싱</span>
+            <span>프론트엔드 개발</span>
+            <span>UXUI 디자인</span>
+            <span>브랜딩 디자인</span>
+          </div>
+
+          <div>
+            <strong>Character</strong>
+            <span>끈기있는</span>
+            <span>도전적인</span>
+            <span>섬세한</span>
+          </div>
+
+          <div>
+            <strong>Tools</strong>
+            <span>HTML5</span>
+            <span>CSS3</span>
+            <span>Sass</span>
+            <span>Media Query</span>
+            <span>jQuery</span>
+            <span>JavaScript</span>
+            <span>React</span>
+            <span>Figma</span>
+            <span>...</span>
+            {/* ... 을 호버하면 포토샵 일러스트도 보이게  */}
+          </div>
+
+          <div>
+            <strong>Experience</strong>
+            <dl>
+              <dt>2022. 04</dt>
+              <dd>웹디자인 기능사</dd>
+              <dt>2018. 01</dt>
+              <dd>GTQ 1급</dd>
+            </dl>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Profile;
