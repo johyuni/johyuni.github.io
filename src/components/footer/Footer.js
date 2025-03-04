9811ab90d304b5b3acebee17194858e86ee56eda
johyuni <2252hyun@gmail.com>
2025-03-04 15:43:48 +0900

    추가 : 푸터스타일 적용

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,24 +7,36 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="inner">
-        <div className="footer__link">
-          <Link to="" target="" className="link">
-            GitHub
-          </Link>
-          <Link to="" target="" className="link">
-            Notion
-          </Link>
-          <Link to="" target="" className="link">
-            Mail
-          </Link>
-          <Link to="" target="" className="link">
-            Phone
-          </Link>
-          <Link to="" target="" className="link">
-            Instagram
-          </Link>
-        </div>
-        <span>&copy;hyyyuni. All rights reserved.</span>
+        <ul className="footer__list">
+          <li className="footer__item">
+            <Link to="" target="" className="link">
+              GitHub
+            </Link>
+          </li>
+          <li className="footer__item">
+            <Link to="" target="" className="link">
+              Notion
+            </Link>
+          </li>
+          <li className="footer__item">
+            <Link to="" target="" className="link">
+              Mail
+            </Link>
+          </li>
+          <li className="footer__item">
+            <Link to="" target="" className="link">
+              Phone
+            </Link>
+          </li>
+          <li className="footer__item">
+            <Link to="" target="" className="link">
+              Instagram
+            </Link>
+          </li>
+        </ul>
+        <span className="footer__copy">
+          &copy;hyyyuni. All rights reserved.
+        </span>
       </div>
     </footer>
   );
