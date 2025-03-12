c70563f0a15ac0afcd8ba349d15f8c09ef09aede
johyuni <2252hyun@gmail.com>
2025-03-12 19:42:48 +0900

    수정 : project 코드 정리리

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -9,7 +9,8 @@ function Project() {
   return (
     <section className="main__project">
       <div className="inner">
-        <strong className="typo-header">프로젝트</strong>
+        <strong className="typo__header">프로젝트</strong>
+
         <div className="content">
           <Swiper
             effect={"fade"}
@@ -22,61 +23,63 @@ function Project() {
             className="mySwiper"
           >
             <SwiperSlide>
-              <div className="slide-text">
-                <strong className="tit">프로젝트 제목 01</strong>
-                <p className="dsc">
+              <div className="typo">
+                <strong className="typo__tit">프로젝트 제목 01</strong>
+                <p className="typo__body3">
                   프로젝트에 대한 간략한 설명 공간입니다. 프로젝트에 대한 간략한
                   설명 공간입니다.프로젝트에 대한 간략한 설명
                   공간입니다.프로젝트에 대한 간략한 설명 공간입니다.
                 </p>
+                <div className="typo__box">
+                  <span className="num">1</span>
+                  <span className="num">5</span>
+                </div>
               </div>
-              <div className="slide-img">
+              <div className="slide__img">
                 <img className="img" src="" alt="프로젝트 대표 이미지 01" />
               </div>
-              <div className="slide-number">
-                <span className="num">1</span>
-                <span className="num">5</span>
-              </div>
             </SwiperSlide>
+
             <SwiperSlide>
-              <div className="slide-text">
-                <strong className="tit">프로젝트 제목 02</strong>
-                <p className="dsc">
+              <div className="typo">
+                <strong className="typo__tit">프로젝트 제목 02</strong>
+                <p className="typo__body3">
                   프로젝트에 대한 간략한 설명 공간입니다. 프로젝트에 대한 간략한
                   설명 공간입니다.프로젝트에 대한 간략한 설명
                   공간입니다.프로젝트에 대한 간략한 설명 공간입니다.
                 </p>
+                <div className="typo__box">
+                  <span className="num">2</span>
+                  <span className="num">5</span>
+                </div>
               </div>
-              <div className="slide-img">
+              <div className="slide__img">
                 <img className="img" src="" alt="프로젝트 대표 이미지 02" />
               </div>
-              <div className="slide-number">
-                <span className="num">2</span>
-                <span className="num">5</span>
-              </div>
             </SwiperSlide>
+
             <SwiperSlide>
-              <div className="slide-text">
-                <strong className="tit">프로젝트 제목 03</strong>
-                <p className="dsc">
+              <div className="typo">
+                <strong className="typo__tit">프로젝트 제목 03</strong>
+                <p className="typo__body3">
                   프로젝트에 대한 간략한 설명 공간입니다. 프로젝트에 대한 간략한
                   설명 공간입니다.프로젝트에 대한 간략한 설명
                   공간입니다.프로젝트에 대한 간략한 설명 공간입니다.
                 </p>
+                <div className="typo__box">
+                  <span className="num">3</span>
+                  <span className="num">5</span>
+                </div>
               </div>
-              <div className="slide-img">
+              <div className="slide__img">
                 <img className="img" src="" alt="프로젝트 대표 이미지 03" />
-              </div>
-              <div className="slide-number">
-                <span className="num">3</span>
-                <span className="num">5</span>
               </div>
             </SwiperSlide>
           </Swiper>
 
-          {/* todo :: 여기에 프로젝트 링크추가 / 호버효과 추가  */}
+          {/* todo :: 여기에 프로젝트 링크추가  */}
           {/* todo :: map으로 돌릴지 고민해보고 개선 */}
-          <div className="slide-link">
+          <div className="slide__link">
             <Link to="" target="" className="link">
               프로젝트 제목 01
             </Link>
