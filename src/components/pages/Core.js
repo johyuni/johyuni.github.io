b1a20beef5a08bc7fe65b3e0c635ea21bfbd37cc
johyuni <2252hyun@gmail.com>
2025-03-13 19:19:05 +0900

    태그 구조 수정 (3)

--- src/components/pages/Core.js
+++ src/components/pages/Core.js
@@ -1,54 +1,52 @@
 import React from "react";
-// todo :: text 클래스 정리해보자 txt-tit, txt-hash
 
 function Core() {
   return (
     <section className="main__core">
       <div className="inner">
-        {/* left*/}
-        <div className="content">
-          <strong className="txt-header">
-            핵심 역량
-            <strong className="tit">
-              같은 목표를 가지면
-              <br />더 멀리 갈 수 있습니다.
-            </strong>
+        <strong className="main__tit">핵심 역량</strong>
+
+        <div className="main__content">
+          <strong className="txt__tit">
+            같이의
+            <br />
+            가치를 믿습니다.
           </strong>
 
-          <ul className="core-list">
-            <li className="core-list__item">
-              <strong className="core-list__tit">소통</strong>
-              <p className="core-list__dsc">
+          <ul className="list">
+            <li className="list__item txt">
+              <strong className="txt__body1">소통</strong>
+              <p className="txt__body2">
                 키워드 상세내용입니다. 키워드 상세내용입니다. 키워드
                 상세내용입니다. 키워드 상세내용입니다. 키워드 상세내용입니다.
                 키워드 상세내용입니다. 키워드 상세내용입니다. 키워드
                 상세내용입니다. 키워드 상세내용입니다. 키워드 상세내용입니다.
               </p>
-              <span className="core-list__hash">
+              <span className="txt__body3">
                 &#35;해시태그 &#35;해시태그 &#35;해시태그
               </span>
             </li>
-            <li className="core-list__item">
-              <strong className="core-list__tit">기술</strong>
-              <p className="core-list__dsc">
+            <li className="list__item txt">
+              <strong className="txt__body1">기술</strong>
+              <p className="txt__body2">
                 키워드 상세내용입니다. 키워드 상세내용입니다. 키워드
                 상세내용입니다. 키워드 상세내용입니다. 키워드 상세내용입니다.
                 키워드 상세내용입니다. 키워드 상세내용입니다. 키워드
                 상세내용입니다. 키워드 상세내용입니다. 키워드 상세내용입니다.
               </p>
-              <span className="core-list__hash">
+              <span className="txt__body3">
                 &#35;해시태그 &#35;해시태그 &#35;해시태그
               </span>
             </li>
-            <li className="core-list__item">
-              <strong className="core-list__tit">상생</strong>
-              <p className="core-list__dsc">
+            <li className="list__item txt">
+              <strong className="txt__body1">상생</strong>
+              <p className="txt__body2">
                 키워드 상세내용입니다. 키워드 상세내용입니다. 키워드
                 상세내용입니다. 키워드 상세내용입니다. 키워드 상세내용입니다.
                 키워드 상세내용입니다. 키워드 상세내용입니다. 키워드
                 상세내용입니다. 키워드 상세내용입니다. 키워드 상세내용입니다.
               </p>
-              <span className="core-list__hash">
+              <span className="txt__body3">
                 &#35;해시태그 &#35;해시태그 &#35;해시태그
               </span>
             </li>
