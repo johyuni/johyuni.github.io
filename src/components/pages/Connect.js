a15cc2a0b9d8ebb1a23afd955fc66f9245e5ea7f
johyuni <2252hyun@gmail.com>
2025-03-13 19:29:43 +0900

    태그 구조 수정 (4)

--- src/components/pages/Connect.js
+++ src/components/pages/Connect.js
@@ -4,17 +4,18 @@ function Connect() {
   return (
     <section className="main__connect">
       <div className="inner">
-        <div className="content">
-          <strong className="tit">저와 함께하고 싶으신가요?</strong>
-          <p className="dsc">
-            배움을 멈출 줄 모르는 든든한 동료가 되겠습니다. 배움을 멈출 줄
-            모르는 든든한 동료가 되겠습니다. <br /> 배움을 멈출 줄 모르는 든든한
-            동료가 되겠습니다. 배움을 멈출 줄 모르는 든든한 동료가 되겠습니다.
-          </p>
-        </div>
+        <div className="main__content">
+          <div className="txt">
+            <strong className="txt__tit">저와 함께하고 싶으신가요?</strong>
+            <p className="txt__body3">
+              배움을 멈출 줄 모르는 든든한 동료가 되겠습니다. 배움을 멈출 줄
+              모르는 든든한 동료가 되겠습니다. <br /> 배움을 멈출 줄 모르는
+              든든한 동료가 되겠습니다. 배움을 멈출 줄 모르는 든든한 동료가
+              되겠습니다.
+            </p>
+          </div>
 
-        <div className="content">
-          <div className="content__img">
+          <div className="imgbox">
             <img className="img" src="" alt="QRcode" />
           </div>
         </div>
