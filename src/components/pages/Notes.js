8b6d9e6073b20b6d91c7bf46155fc524d04459e2
johyuni <2252hyun@gmail.com>
2025-03-19 17:20:57 +0900

    수정 : note 리스트 세로 스크롤처리, 자동넘버링

--- src/components/pages/Notes.js
+++ src/components/pages/Notes.js
@@ -5,20 +5,22 @@ function Notes() {
   // list 배열 생성
   const list = [
     {
-      id: "(1)",
       name: "Design System",
       link: "https://velog.io/@hyyyuni/posts?tag=BEM",
       dsc: "포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 ",
     },
     {
-      id: "(2)",
       name: "Clone-Coding",
       link: "https://velog.io/@hyyyuni/posts?tag=localhost",
       dsc: "포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 ",
     },
     {
-      id: "(3)",
       name: "Code Editor",
+      link: "",
+      dsc: "포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 ",
+    },
+    {
+      name: "Git",
       link: "",
       dsc: "포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 포스트 설명 한줄 말줄임 ",
     },
@@ -53,10 +55,10 @@ function Notes() {
 
           {/* list */}
           <ul className="list">
-            {list.map((item) => (
+            {list.map((item, index) => (
               <li key={item.name} className="list__item">
                 <Link to={item.link} className="txt">
-                  <span className="txt__body4">{item.id}</span>
+                  <span className="txt__body4">({index + 1})</span>
                   <strong className="txt__body1">{item.name}</strong>
                   <span className="txt__body3">{item.dsc}</span>
                   <i className="ico"></i>
