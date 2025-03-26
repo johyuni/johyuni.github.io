877f8b96437852102423655df199075d18be20d5
johyuni <2252hyun@gmail.com>
2025-03-26 15:39:31 +0900

    수정 : 햄버거 메뉴 스타일링

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,75 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
-      {/* gnb--pc*/}
-      <div className="gnb gnb--pc">
+      {/* gnb*/}
+      <div className="gnb">
         <a href="/" role="button" aria-label="상위로 이동">
-          <h1 className="header__logo">
+          <h1 className="gnb__logo">
             <span className="blind">Portfolio</span>
           </h1>
         </a>
 
         {/* nav */}
-        <nav className="header__nav">
-          <a
-            href="#profile"
-            role="button"
-            className="header__link"
-            aria-label="프로필"
-          >
-            Profile
-          </a>
-          <a
-            href="#project"
-            role="button"
-            className="header__link"
-            aria-label="프로젝트"
-          >
-            Project
-          </a>
-          <a
-            href="#notes"
-            role="button"
-            className="header__link"
-            aria-label="노트"
-          >
-            Notes
-          </a>
-          <a
-            href="#core"
-            role="button"
-            className="header__link"
-            aria-label="핵심"
-          >
-            Core
-          </a>
-          <a
-            href="#connect"
-            role="button"
-            className="header__link"
-            aria-label="연결"
-          >
-            Connect
-          </a>
-        </nav>
-        <button
-          type="button"
-          aria-label="메뉴 열기"
-          class="btn ta_view"
-        ></button>
-      </div>
-
-      {/* gnb--mobile*/}
-      {/* 태블릿부터 풀메뉴노출 */}
-      <ul className="gnb gnb--mobile">
-        <li className="gnb--mobile__depth">
+        <nav className="gnb__nav">
           <a href="#profile" role="button" className="link" aria-label="프로필">
             Profile
           </a>
-        </li>
-        <li className="gnb--mobile__depth">
           <a
             href="#project"
             role="button"
@@ -78,23 +25,125 @@ function Header() {
           >
             Project
           </a>
-        </li>
-        <li className="gnb--mobile__depth">
           <a href="#notes" role="button" className="link" aria-label="노트">
             Notes
           </a>
-        </li>
-        <li className="gnb--mobile__depth">
           <a href="#core" role="button" className="link" aria-label="핵심">
             Core
           </a>
-        </li>
-        <li className="gnb--mobile__depth">
           <a href="#connect" role="button" className="link" aria-label="연결">
             Connect
           </a>
-        </li>
-      </ul>
+        </nav>
+
+        {/* 이 아이는 태블릿에서부터 노출 */}
+        <button
+          type="button"
+          aria-label="메뉴 열기"
+          className="gnb__btn"
+        ></button>
+      </div>
+
+      {/* gnb--mobile*/}
+      {/* 태블릿부터 풀메뉴노출 */}
+      <div className="gnb gnb--full">
+        <div className="gnb--full__header">
+          <a href="/" role="button" aria-label="상위로 이동">
+            <h1 className="gnb__logo">
+              <span className="blind">Portfolio</span>
+            </h1>
+          </a>
+          <button
+            type="button"
+            aria-label="메뉴 열기"
+            className="gnb__btn"
+          ></button>
+        </div>
+
+        <ul className="gnb--full__menu">
+          <li className="gnb--full__depth">
+            <a
+              href="#profile"
+              role="button"
+              className="link"
+              aria-label="프로필"
+            >
+              프로필
+            </a>
+          </li>
+          <li className="gnb--full__depth">
+            <a
+              href="#project"
+              role="button"
+              className="link"
+              aria-label="프로젝트"
+            >
+              프로젝트
+            </a>
+          </li>
+          <li className="gnb--full__depth">
+            <a href="#notes" role="button" className="link" aria-label="노트">
+              노트
+            </a>
+          </li>
+          <li className="gnb--full__depth">
+            <a href="#core" role="button" className="link" aria-label="핵심">
+              핵심역량
+            </a>
+          </li>
+          <li className="gnb--full__depth">
+            <a href="#connect" role="button" className="link" aria-label="연결">
+              연결
+            </a>
+          </li>
+        </ul>
+
+        <div className="gnb--full__footer">
+          <ul className="gnb--full__list">
+            <li className="gnb--full__item">
+              <Link
+                to="https://github.com/johyuni"
+                target="_blank"
+                className="link"
+              >
+                GitHub
+              </Link>
+            </li>
+            <li className="gnb--full__item">
+              <Link
+                to="https://velog.io/@hyyyuni/posts"
+                target="_blank"
+                className="link"
+              >
+                Velog
+              </Link>
+            </li>
+            <li className="gnb--full__item">
+              <a href="mailto:2252hyun@gmail.com" className="link">
+                Mail
+              </a>
+              {/*  todo :: 메일은 다시 테스트 필요함 */}
+            </li>
+            <li className="gnb--full__item">
+              <Link to="tel:01040682252" className="link">
+                Phone
+              </Link>
+            </li>
+            <li className="gnb--full__item">
+              <Link
+                to="https://www.instagram.com/iiuoahy?igsh=bmt5MTk1eDB0emho&utm_source=qr"
+                target="_blank"
+                className="link"
+              >
+                Instagram
+              </Link>
+            </li>
+          </ul>
+          <span className="gnb--full__copy">
+            &copy;hyyyuni. All rights reserved.
+          </span>
+        </div>
+      </div>
     </header>
   );
 }
